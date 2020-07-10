d286b8f31489cc0dd9d7da214aad93d129928731
Michael R <mruhaak@gmail.com>
2020-07-10 02:25:36 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,14 @@
 class Shoe
-attr_accessor :color, :size, :material, :condition
-attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
-def initialize(brand)
-  @brand = brand
-end
+  def initialize(brand)
+    @brand = brand
+  end
 
-def cobble
-  @condition = "new"
-  puts "Your shoe is as good as new!"
-end
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
 end
 #learn spec/02_shoe_spec.rb
