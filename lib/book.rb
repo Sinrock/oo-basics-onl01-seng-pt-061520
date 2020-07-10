e97ae2c0cb0f57e35e2a0708036813940f283542
Michael R <mruhaak@gmail.com>
2020-07-10 02:04:58 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,20 +1,31 @@
 class Book
   def initialize(title)
-    @title = title
+    @title = title #Start Setters
   end
 def author=(author)
   @author = author
 end
-
+def page_count=(num)
+  @page_count = num
+end
+def genre=(genre)
+  @genre = genre
+end
+#Start Getters
 def title
   @title
 end
-
+def author
+  @author
+end
+def page_count
+  @page_count
+end
+def genre
+  @genre
 end
 
-
-
-
+end
 
 
 #learn spec/01_book_spec.rb
